894fa2fedf91ed47e30756569fe85b84a185c3b5
Ericgacoki <gacokieric@gmail.com>
2022-08-05 13:55:39 +0300

    add colors

---
+++ app/src/main/java/com/ericg/kripto/presentation/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.ericg.kripto.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkGray = Color(0xFF202020)
+val MediumGray = Color(0xFF505050)
+val ColorPrimary = Color(0xFF122E4E)
+val TextWhite = Color(0xFFEEEEEE)
+val ColorDormantBg = Color(0XFFFFECEC)
+val ColorDormant = Color(0XFF764F4F)
+val ColorBadgeBg = Color(0XFF039800).copy(alpha = 0.24F)
+val ColorBadgeText = Color(0xFF027600)
