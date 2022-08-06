0707eaccdf5b9866659e7b0a9a9bc802454bf00a
Ericgacoki <gacokieric@gmail.com>
2022-08-06 11:51:15 +0300

    add coin dto

---
+++ app/src/main/java/com/ericg/kripto/data/remote/dto/CoinDto.kt
@@ -0,0 +1,16 @@
+package com.ericg.kripto.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CoinDto(
+    val id: String,
+    val name: String,
+    val symbol: String,
+    val rank: Int,
+    @SerializedName("is_new")
+    val isNew: Boolean,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    val type: String
+)
