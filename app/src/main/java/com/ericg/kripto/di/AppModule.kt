730d8394d3ab6fcfbd07dc9d5bab63f4f0324079
Ericgacoki <gacokieric@gmail.com>
2022-08-06 11:50:38 +0300

    add app module

---
+++ app/src/main/java/com/ericg/kripto/di/AppModule.kt
@@ -0,0 +1,51 @@
+package com.ericg.kripto.di
+
+import com.ericg.kripto.data.remote.ApiService
+import com.ericg.kripto.domain.repository.KriptoRepository
+import com.ericg.kripto.domain.use_case.get_coins.GetCoinsUseCase
+import com.ericg.kripto.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object AppModule {
+    @Singleton
+    @Provides
+    fun providesOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        return OkHttpClient.Builder()
+            .addInterceptor(interceptor)
+            .callTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .writeTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun providesApiService(httpClient: OkHttpClient): ApiService {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(httpClient)
+            .build()
+            .create()
+    }
+
+    @Provides
+    @Singleton
+    fun providesCoinsUseCase(repository: KriptoRepository): GetCoinsUseCase{
+        return GetCoinsUseCase(repository)
+    }
+}
