5af9c1c2189f54c6024b1eac059d75c957505ba1
Ericgacoki <gacokieric@gmail.com>
2022-08-06 11:51:35 +0300

    add coin link

---
+++ app/src/main/java/com/ericg/kripto/domain/model/CoinDetailLink.kt
@@ -0,0 +1,11 @@
+package com.ericg.kripto.domain.model
+
+
+data class CoinDetailLink(
+    val explorer: List<String>,
+    val facebook: List<String>,
+    val reddit: List<String>,
+    val sourceCode: List<String>,
+    val website: List<String>,
+    val youtube: List<String>
+)
