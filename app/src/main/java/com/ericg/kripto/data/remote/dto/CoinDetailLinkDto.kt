4a6845091ce88da8dfd3d7539d6d4d5f7f088836
Ericgacoki <gacokieric@gmail.com>
2022-08-06 11:51:44 +0300

    add coin link dto

---
+++ app/src/main/java/com/ericg/kripto/data/remote/dto/CoinDetailLinkDto.kt
@@ -0,0 +1,14 @@
+package com.ericg.kripto.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CoinDetailLinkDto(
+    val explorer: List<String>,
+    val facebook: List<String>,
+    val reddit: List<String>,
+    @SerializedName("source_code")
+    val sourceCode: List<String>,
+    val website: List<String>,
+    val youtube: List<String>
+)
