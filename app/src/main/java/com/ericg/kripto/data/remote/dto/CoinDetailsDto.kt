207a69e14621acca5fc3f05f30aade04983d92fa
Ericgacoki <gacokieric@gmail.com>
2022-08-06 11:51:52 +0300

    add coin details dto

---
+++ app/src/main/java/com/ericg/kripto/data/remote/dto/CoinDetailsDto.kt
@@ -0,0 +1,42 @@
+package com.ericg.kripto.data.remote.dto
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CoinDetailsDto(
+    val id: String,
+    val name: String,
+    val symbol: String,
+    val rank: Int,
+    @SerializedName("is_new")
+    val isNew: Boolean,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    val type: String,
+    val tagDtos: List<TagDto>,
+    val team: List<TeamMemberDto>,
+    val description: String,
+    val message: String,
+    @SerializedName("open_source")
+    val openSource: Boolean,
+    @SerializedName("started_at")
+    val startedAt: String,
+    @SerializedName("development_status")
+    val developmentStatus: String,
+    @SerializedName("hardware_wallet")
+    val hardwareWallet: Boolean,
+    @SerializedName("proof_type")
+    val proofType: String,
+    @SerializedName("org_structure")
+    val orgStructure: String,
+    @SerializedName("hash_algorithm")
+    val hashAlgorithm: String,
+    val links: CoinDetailLinkDto,
+    @SerializedName("links_extended")
+    val linksExtendedDto: List<LinksExtendedDto>,
+    val whitepaperDto: WhitepaperDto,
+    @SerializedName("first_data_at")
+    val firstDataAt: String,
+    @SerializedName("last_data_at")
+    val lastDataAt: String
+)
