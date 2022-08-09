234502871d454ece22dfa6792b28232fb2019c7a
Ericgacoki <gacokieric@gmail.com>
2022-08-09 11:54:15 +0300

    add coin details screen

---
+++ app/src/main/java/com/ericg/kripto/presentation/screen/coin_details/component/CoinDetailsScreen.kt
@@ -0,0 +1,10 @@
+package com.ericg.kripto.presentation.screen.coin_details.component
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun CoinDetailsScreen() {
+
+}
