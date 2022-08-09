cc4f880385a4d214573f22300202f4cf14679748
Ericgacoki <gacokieric@gmail.com>
2022-08-09 11:55:39 +0300

    add exchange details

---
+++ app/src/main/java/com/ericg/kripto/domain/model/ExchangeDetails.kt
@@ -0,0 +1,22 @@
+package com.ericg.kripto.domain.model
+
+data class ExchangeDetails(
+    val id: String,
+    val name: String,
+    val description: String,
+    val active: Boolean,
+    val websiteStatus: Boolean,
+    val apiStatus: Boolean,
+    val message: Any?,
+    val links: Links,
+    val marketsDataFetched: Boolean,
+    val adjustedRank: Int,
+    val reportedRank: Int,
+    val currencies: Int,
+    val markets: Int,
+    val fiats: List<Any?>,
+    val quotes: Quotes,
+    val lastUpdated: String,
+    val imgRev: Int,
+    val confidenceScore: Int
+)
