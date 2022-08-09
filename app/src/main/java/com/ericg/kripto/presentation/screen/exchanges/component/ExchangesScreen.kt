0f2dd63821626b45e144192bd4dfdfe8f18b25c6
Ericgacoki <gacokieric@gmail.com>
2022-08-09 11:56:27 +0300

    add exchange screen

---
+++ app/src/main/java/com/ericg/kripto/presentation/screen/exchanges/component/ExchangesScreen.kt
@@ -0,0 +1,10 @@
+package com.ericg.kripto.presentation.screen.exchanges.component
+
+import androidx.compose.runtime.Composable
+import com.ramcosta.composedestinations.annotation.Destination
+
+@Destination
+@Composable
+fun ExchangesScreen() {
+
+}
