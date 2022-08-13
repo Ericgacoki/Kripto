f17857ee8f168c35ae43cdd124605c823b08f425
Ericgacoki <gacokieric@gmail.com>
2022-08-13 14:10:36 +0300

    add USD model

---
+++ app/src/main/java/com/ericg/kripto/domain/model/USD.kt
@@ -0,0 +1,10 @@
+package com.ericg.kripto.domain.model
+
+data class USD(
+    val reportedVolume24h: Int,
+    val adjustedVolume24h: Int,
+    val reportedVolume7d: Double,
+    val adjustedVolume7d: Double,
+    val reportedVolume30d: Double,
+    val adjustedVolume30d: Double
+)
