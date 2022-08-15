a129335dcd4cf27744ebdb9c9e820f6eae2dcb31
Ericgacoki <gacokieric@gmail.com>
2022-08-15 23:09:21 +0300

    remove unused attribute

--- app/src/main/java/com/ericg/kripto/presentation/ui/sharedComposables/BottomNavItem.kt
+++ app/src/main/java/com/ericg/kripto/presentation/ui/sharedComposables/BottomNavItem.kt
@@ -1,4 +1,4 @@
-package com.ericg.kripto.presentation.ui
+package com.ericg.kripto.presentation.ui.sharedComposables
 
 import com.ericg.kripto.R
 import com.ericg.kripto.presentation.screen.destinations.CoinLIstScreenDestination
@@ -7,25 +7,21 @@ import com.ericg.kripto.presentation.screen.destinations.ExchangesScreenDestinat
 import com.ericg.kripto.presentation.screen.destinations.PriceConversionScreenDestination
 
 sealed class BottomNavItem(
-    val title: String,
     val icon: Int,
     val destination: Destination
 ) {
 
     object CoinList : BottomNavItem(
-        title = "Coin",
         icon = R.drawable.ic_coin,
         destination = CoinLIstScreenDestination
     )
 
     object Exchanges : BottomNavItem(
-        title = "Market",
         icon = R.drawable.ic_exchange,
         destination = ExchangesScreenDestination
     )
 
     object PriceConversion : BottomNavItem(
-        title = "Rate",
         icon = R.drawable.ic_convert,
         destination = PriceConversionScreenDestination
     )
