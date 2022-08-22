465277de5d6d2a1c801024819cf6235291e62e72
Ericgacoki <gacokieric@gmail.com>
2022-08-22 23:10:38 +0300

    make attr nullable

--- app/src/main/java/com/ericg/kripto/data/remote/dto/CoinDetailsDto.kt
+++ app/src/main/java/com/ericg/kripto/data/remote/dto/CoinDetailsDto.kt
@@ -4,39 +4,40 @@ package com.ericg.kripto.data.remote.dto
 import com.google.gson.annotations.SerializedName
 
 data class CoinDetailsDto(
-    val id: String,
-    val name: String,
-    val symbol: String,
-    val rank: Int,
+    val id: String?,
+    val name: String?,
+    val symbol: String?,
+    val rank: Int?,
     @SerializedName("is_new")
-    val isNew: Boolean,
+    val isNew: Boolean?,
     @SerializedName("is_active")
-    val isActive: Boolean,
-    val type: String,
-    val tagDtos: List<TagDto>,
-    val team: List<TeamMemberDto>,
-    val description: String,
-    val message: String,
+    val isActive: Boolean?,
+    val type: String?,
+    @SerializedName("tags")
+    val tagDtos: List<TagDto>?,
+    val team: List<TeamMemberDto>?,
+    val description: String?,
+    val message: String?,
     @SerializedName("open_source")
-    val openSource: Boolean,
+    val openSource: Boolean?,
     @SerializedName("started_at")
-    val startedAt: String,
+    val startedAt: String?,
     @SerializedName("development_status")
-    val developmentStatus: String,
+    val developmentStatus: String?,
     @SerializedName("hardware_wallet")
-    val hardwareWallet: Boolean,
+    val hardwareWallet: Boolean?,
     @SerializedName("proof_type")
-    val proofType: String,
+    val proofType: String?,
     @SerializedName("org_structure")
-    val orgStructure: String,
+    val orgStructure: String?,
     @SerializedName("hash_algorithm")
-    val hashAlgorithm: String,
-    val links: CoinDetailLinkDto,
+    val hashAlgorithm: String?,
+    val links: CoinDetailLinkDto?,
     @SerializedName("links_extended")
-    val linksExtendedDto: List<LinksExtendedDto>,
-    val whitepaperDto: WhitepaperDto,
+    val linksExtendedDto: List<LinksExtendedDto>?,
+    val whitepaperDto: WhitepaperDto?,
     @SerializedName("first_data_at")
-    val firstDataAt: String,
+    val firstDataAt: String?,
     @SerializedName("last_data_at")
-    val lastDataAt: String
+    val lastDataAt: String?
 )
