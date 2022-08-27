ebfdca045731b4fe5cf31a1b3aa920bf0fcbf7ed
Ericgacoki <gacokieric@gmail.com>
2022-08-27 23:12:31 +0300

    add app top bar

--- app/src/main/java/com/ericg/kripto/presentation/screen/exchanges/component/ExchangesScreen.kt
+++ app/src/main/java/com/ericg/kripto/presentation/screen/exchanges/component/ExchangesScreen.kt
@@ -1,10 +1,31 @@
 package com.ericg.kripto.presentation.screen.exchanges.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.ericg.kripto.presentation.ui.sharedComposables.AppTopBar
 import com.ramcosta.composedestinations.annotation.Destination
 
 @Destination
 @Composable
 fun ExchangesScreen() {
-
+    Scaffold(
+        modifier = Modifier
+            .background(Color.White)
+            .fillMaxSize(),
+        topBar = {
+            AppTopBar(
+                title = "Exchanges",
+                showSearchBar = true,
+                onSearchParamChange = { newParam ->
+                    // TODO: Initiate a search event
+                }
+            )
+        }
+    ) {
+        val unUsedPd = it
+    }
 }
