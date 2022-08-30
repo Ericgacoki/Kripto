a50b3a7e2d422c0312b7416d7e4b169d960bbe32
Ericgacoki <gacokieric@gmail.com>
2022-08-30 23:14:54 +0300

    add team member item

---
+++ app/src/main/java/com/ericg/kripto/presentation/screen/coin_details/component/TeamMemberItem.kt
@@ -0,0 +1,56 @@
+package com.ericg.kripto.presentation.screen.coin_details.component
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ericg.kripto.R
+import com.ericg.kripto.presentation.theme.ColorPrimary
+
+@Composable
+fun TeamMemberItem(name: String, position: String) {
+    Row(
+        modifier = Modifier.padding(vertical = 8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.ic_person_icon),
+            tint = ColorPrimary.copy(alpha = 0.56F),
+            modifier = Modifier
+                .size(48.dp)
+                .clip(shape = CircleShape),
+            contentDescription = "Person Icon"
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(start = 8.dp)
+                .height(48.dp),
+            horizontalAlignment = Alignment.Start,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+            Text(
+                color = ColorPrimary,
+                text = name,
+                fontSize = 14.sp,
+                fontWeight = FontWeight.Normal
+            )
+            Text(
+                color = ColorPrimary.copy(alpha = 0.56F),
+                text = position,
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Normal
+            )
+        }
+    }
+}
+
