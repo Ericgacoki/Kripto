e8938239233bdee2842d80cab032811bab27d274
Ericgacoki <gacokieric@gmail.com>
2022-08-30 23:14:35 +0300

    add retry button

---
+++ app/src/main/java/com/ericg/kripto/presentation/ui/sharedComposables/RetryButton.kt
@@ -0,0 +1,38 @@
+package com.ericg.kripto.presentation.ui.sharedComposables
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.ericg.kripto.presentation.theme.ColorDormantBg
+
+@Composable
+fun RetryButton(error: String, onRetryEvent: () -> Unit) {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                modifier = Modifier.padding(4.dp),
+                text = error,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Button(onClick = {
+                onRetryEvent()
+            }) {
+                Text(
+                    text = "RETRY",
+                    color = ColorDormantBg
+                )
+            }
+        }
+    }
+}
