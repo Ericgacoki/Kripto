d9e8bc9367e8dac14da9236a9d5ae007e15dd730
Ericgacoki <gacokieric@gmail.com>
2022-09-01 21:22:11 +0300

    update colors

--- app/src/main/java/com/ericg/kripto/presentation/theme/Color.kt
+++ app/src/main/java/com/ericg/kripto/presentation/theme/Color.kt
@@ -8,7 +8,9 @@ val ColorPrimary = Color(0xFF122E4E)
 val TextWhite = Color(0xFFEEEEEE)
 val ColorDormantBg = Color(0XFFFFECEC)
 val ColorDormant = Color(0XFF764F4F)
-val ColorBadgeBg = Color(0XFF039800).copy(alpha = 0.24F)
+val ColorBadgeBg = Color(0XFF039800).copy(alpha = 0.12F)
 val ColorBadgeText = Color(0xFF027600)
-val ColorLink = Color(0XFF85B6FF)
-val ColorOrange = Color(0XFFF7941E)
+val ColorLinkDark = Color(0xFF6AA4FA)
+val ColorOrangeDark = Color(0xFFFF9517)
+
+// TODO: Standardize colors -> Set opacity and names here
