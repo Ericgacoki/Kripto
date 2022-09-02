44ef1792791b7ef1768838ab82c5b6f1e0ffb514
Ericgacoki <gacokieric@gmail.com>
2022-09-02 21:27:33 +0300

    add no match found lottie

---
+++ app/src/main/java/com/ericg/kripto/presentation/ui/sharedComposables/NoMatchFound.kt
@@ -0,0 +1,19 @@
+package com.ericg.kripto.presentation.ui.sharedComposables
+
+import androidx.annotation.RawRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.ericg.kripto.util.LottieLoader
+
+@Composable
+fun NoMatchFound(@RawRes lottie: Int) {
+    Box(modifier = Modifier.fillMaxHeight(0.9F).fillMaxWidth(), contentAlignment = Alignment.Center) {
+        LottieLoader(lottieFile = lottie, modifier = Modifier.size(250.dp))
+    }
+}
