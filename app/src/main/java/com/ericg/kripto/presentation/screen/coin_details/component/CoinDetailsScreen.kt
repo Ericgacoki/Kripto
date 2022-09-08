d69cd23e0fe32249a54454014bccaa2dd56054de
Ericgacoki <gacokieric@gmail.com>
2022-09-08 13:26:41 +0300

    remove overscroll effect

--- app/src/main/java/com/ericg/kripto/presentation/screen/coin_details/component/CoinDetailsScreen.kt
+++ app/src/main/java/com/ericg/kripto/presentation/screen/coin_details/component/CoinDetailsScreen.kt
@@ -2,6 +2,7 @@ package com.ericg.kripto.presentation.screen.coin_details.component
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -28,6 +29,7 @@ import com.ericg.kripto.presentation.theme.ColorBadgeText
 import com.ericg.kripto.presentation.theme.ColorLinkDark
 import com.ericg.kripto.presentation.theme.ColorOrangeDark
 import com.ericg.kripto.presentation.theme.ColorPrimary
+import com.ericg.kripto.presentation.ui.sharedComposables.LinkItem
 import com.ericg.kripto.presentation.ui.sharedComposables.RetryButton
 import com.ericg.kripto.util.GifImageLoader
 import com.google.accompanist.flowlayout.FlowRow
@@ -76,179 +78,182 @@ fun CoinDetailsScreen(
             }
         }
     ) {
-
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            scaffoldState = scaffoldState,
-            topBar = {
-                CoinDetailsTopBar(
-                    name = name,
-                    symbol = symbol,
-                    rank = rank,
-                    type = type,
-                    isNew = isNew
-                )
-            }
-        ) { pdValues ->
-            val pd = pdValues // ignore padding values
-
-            val coinDetailsState = coinDetailsViewModel.state.collectAsState()
-
-            LaunchedEffect(key1 = Unit) {
-                coinDetailsViewModel.onGetCoinDetailsEvent(id)
-            }
-
-            if (coinDetailsState.value.isLoading) {
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    GifImageLoader(
-                        modifier = Modifier.size(250.dp),
-                        resource = R.raw.kripto_loading
+        CompositionLocalProvider(
+            LocalOverscrollConfiguration provides null
+        ) {
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState,
+                topBar = {
+                    CoinDetailsTopBar(
+                        name = name,
+                        symbol = symbol,
+                        rank = rank,
+                        type = type,
+                        isNew = isNew
                     )
                 }
-            } else if (coinDetailsState.value.coinDetails?.id.isNullOrEmpty().not()) {
-                LazyColumn(
-                    modifier = Modifier.padding(
-                        start = 12.dp,
-                        end = 12.dp,
-                        bottom = 12.dp
-                    )
-                ) {
-                    item {
-                        coinDetailsState.value.coinDetails?.description.let { desc ->
-                            Text(
-                                text = if (desc.isNullOrEmpty()) "No description" else desc,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(bottom = 6.dp),
-                                fontSize = 14.sp,
-                                color = ColorPrimary.copy(alpha = .74F),
-                                fontWeight = FontWeight.Normal
-                            )
-                        }
+            ) { pdValues ->
+                val pd = pdValues // ignore padding values
+
+                val coinDetailsState = coinDetailsViewModel.state.collectAsState()
+
+                LaunchedEffect(key1 = Unit) {
+                    coinDetailsViewModel.onGetCoinDetailsEvent(id)
+                }
+
+                if (coinDetailsState.value.isLoading) {
+                    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                        GifImageLoader(
+                            modifier = Modifier.size(250.dp),
+                            resource = R.raw.kripto_loading
+                        )
                     }
-
-                    val tags = coinDetailsState.value.coinDetails?.tags ?: emptyList<Tag>()
-                    if (tags.isNotEmpty())
-                        stickyHeader {
-                            Text(
-                                modifier = Modifier
-                                    .padding(bottom = 4.dp)
-                                    .fillMaxWidth()
-                                    .background(Color.White),
-                                color = ColorPrimary,
-                                text = "Tags",
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.SemiBold
-                            )
-                        }
-
-                    if (tags.isNotEmpty())
+                } else if (coinDetailsState.value.coinDetails?.id.isNullOrEmpty().not()) {
+                    LazyColumn(
+                        modifier = Modifier.padding(
+                            start = 12.dp,
+                            end = 12.dp,
+                            bottom = 12.dp
+                        )
+                    ) {
                         item {
-                            val colors =
-                                listOf<Color>(
-                                    ColorPrimary,
-                                    ColorLinkDark,
-                                    ColorOrangeDark,
-                                    ColorBadgeText
+                            coinDetailsState.value.coinDetails?.description.let { desc ->
+                                Text(
+                                    text = if (desc.isNullOrEmpty()) "No description" else desc,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(bottom = 6.dp),
+                                    fontSize = 14.sp,
+                                    color = ColorPrimary.copy(alpha = .74F),
+                                    fontWeight = FontWeight.Normal
                                 )
-
-                            FlowRow(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(bottom = 8.dp),
-                                mainAxisSpacing = 8.dp,
-                                crossAxisSpacing = 8.dp
-                            ) {
-                                tags.sortedBy { it.name.length }.forEach {
-                                    TagItem(name = it.name, colors = colors)
-                                }
                             }
                         }
 
-                    val links = coinDetailsState.value.coinDetails?.links
-                    val validLinks = mapOf<String, List<String>?>(
-                        "Explorer" to links?.explorer,
-                        "Website" to links?.website,
-                        "Source code" to links?.sourceCode,
-                        "YouTube" to links?.youtube,
-                        "Facebook" to links?.facebook,
-                        "Reddit" to links?.reddit
-                    ).filter {
-                        it.value != null && it.let { list ->
-                            list.value?.isNotEmpty() ?: false
-                        }
-                    }.toList()
+                        val tags = coinDetailsState.value.coinDetails?.tags ?: emptyList<Tag>()
+                        if (tags.isNotEmpty())
+                            stickyHeader {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(bottom = 4.dp)
+                                        .fillMaxWidth()
+                                        .background(Color.White),
+                                    color = ColorPrimary,
+                                    text = "Tags",
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            }
 
-                    if (validLinks.isNotEmpty())
-                        stickyHeader {
-                            Text(
-                                modifier = Modifier
-                                    .padding(bottom = 4.dp)
-                                    .fillMaxWidth()
-                                    .background(Color.White),
-                                color = ColorPrimary,
-                                text = "Links",
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.SemiBold
-                            )
-                        }
+                        if (tags.isNotEmpty())
+                            item {
+                                val colors =
+                                    listOf<Color>(
+                                        ColorPrimary,
+                                        ColorLinkDark,
+                                        ColorOrangeDark,
+                                        ColorBadgeText
+                                    )
 
-                    if (validLinks.isNotEmpty())
-                        item {
-                            LazyRow(modifier = Modifier.padding(bottom = 4.dp),
-                                content = {
-                                    items(validLinks) { pair ->
-                                        LinkItem(linksPair = pair) {
-                                            if (it.size > 1) {
-                                                coroutineScope.launch {
-                                                    bottomSheetContentState =
-                                                        bottomSheetContentState.copy(links = it)
-                                                    modalBottomSheetState.show()
+                                FlowRow(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(bottom = 8.dp),
+                                    mainAxisSpacing = 8.dp,
+                                    crossAxisSpacing = 8.dp
+                                ) {
+                                    tags.sortedBy { it.name.length }.forEach {
+                                        TagItem(name = it.name, colors = colors)
+                                    }
+                                }
+                            }
+
+                        val links = coinDetailsState.value.coinDetails?.links
+                        val validLinks = mapOf<String, List<String>?>(
+                            "Explorer" to links?.explorer,
+                            "Website" to links?.website,
+                            "Source code" to links?.sourceCode,
+                            "YouTube" to links?.youtube,
+                            "Facebook" to links?.facebook,
+                            "Reddit" to links?.reddit
+                        ).filter {
+                            it.value != null && it.let { list ->
+                                list.value?.isNotEmpty() ?: false
+                            }
+                        }.toList()
+
+                        if (validLinks.isNotEmpty())
+                            stickyHeader {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(bottom = 4.dp)
+                                        .fillMaxWidth()
+                                        .background(Color.White),
+                                    color = ColorPrimary,
+                                    text = "Links",
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            }
+
+                        if (validLinks.isNotEmpty())
+                            item {
+                                LazyRow(modifier = Modifier.padding(bottom = 4.dp),
+                                    content = {
+                                        items(validLinks) { pair ->
+                                            LinkItem(linksPair = pair) {
+                                                if (it.size > 1) {
+                                                    coroutineScope.launch {
+                                                        bottomSheetContentState =
+                                                            bottomSheetContentState.copy(links = it)
+                                                        modalBottomSheetState.show()
+                                                    }
+                                                } else {
+                                                    uriHandler.openUri(it[0])
                                                 }
-                                            } else {
-                                                uriHandler.openUri(it[0])
                                             }
                                         }
+                                    })
+                            }
+
+                        val members =
+                            coinDetailsState.value.coinDetails?.team ?: emptyList<TeamMember>()
+                        if (members.isNotEmpty())
+                            stickyHeader {
+                                Text(
+                                    modifier = Modifier
+                                        .padding(bottom = 4.dp)
+                                        .fillMaxWidth()
+                                        .background(Color.White),
+                                    color = ColorPrimary,
+                                    text = "Team",
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.SemiBold
+                                )
+                            }
+
+                        if (members.isNotEmpty())
+                            item {
+                                Column(
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .wrapContentHeight()
+                                ) {
+                                    members.forEach { member ->
+                                        TeamMemberItem(member.name, member.position)
                                     }
-                                })
-                        }
-
-                    val members =
-                        coinDetailsState.value.coinDetails?.team ?: emptyList<TeamMember>()
-                    if (members.isNotEmpty())
-                        stickyHeader {
-                            Text(
-                                modifier = Modifier
-                                    .padding(bottom = 4.dp)
-                                    .fillMaxWidth()
-                                    .background(Color.White),
-                                color = ColorPrimary,
-                                text = "Team",
-                                fontSize = 18.sp,
-                                fontWeight = FontWeight.SemiBold
-                            )
-                        }
-
-                    if (members.isNotEmpty())
-                        item {
-                            Column(
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .wrapContentHeight()
-                            ) {
-                                members.forEach { member ->
-                                    TeamMemberItem(member.name, member.position)
                                 }
                             }
-                        }
-                }
-            } else {
-                RetryButton(
-                    error = coinDetailsState.value.error,
-                    onRetryEvent = {
-                        coinDetailsViewModel.onGetCoinDetailsEvent(id)
                     }
-                )
+                } else {
+                    RetryButton(
+                        error = coinDetailsState.value.error,
+                        onRetryEvent = {
+                            coinDetailsViewModel.onGetCoinDetailsEvent(id)
+                        }
+                    )
+                }
             }
         }
     }
