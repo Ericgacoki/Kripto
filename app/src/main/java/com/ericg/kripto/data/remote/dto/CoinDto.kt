d07e173e1a8cb7fa06c2b68f774f3a0f3c0c0513
Ericgacoki <gacokieric@gmail.com>
2022-09-08 13:27:05 +0300

    remove entity annotation from dto

--- app/src/main/java/com/ericg/kripto/data/remote/dto/CoinDto.kt
+++ app/src/main/java/com/ericg/kripto/data/remote/dto/CoinDto.kt
@@ -1,12 +1,8 @@
 package com.ericg.kripto.data.remote.dto
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
-@Entity(tableName = "coins")
 data class CoinDto(
-    @PrimaryKey(autoGenerate = false)
     val id: String,
     val name: String,
     val symbol: String,
