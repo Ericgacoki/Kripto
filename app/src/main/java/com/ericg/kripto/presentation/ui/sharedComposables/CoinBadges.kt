1cf455c7734fccafe58667d9d5db8641419d8092
Ericgacoki <gacokieric@gmail.com>
2022-10-13 10:00:40 +0300

    update coin badge

--- app/src/main/java/com/ericg/kripto/presentation/ui/sharedComposables/CoinBadges.kt
+++ app/src/main/java/com/ericg/kripto/presentation/ui/sharedComposables/CoinBadges.kt
@@ -1,62 +1,63 @@
 package com.ericg.kripto.presentation.ui.sharedComposables
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme.colorScheme
+import androidx.compose.material3.MaterialTheme.typography
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.ericg.kripto.presentation.theme.ColorBadgeBg
-import com.ericg.kripto.presentation.theme.ColorBadgeText
-import com.ericg.kripto.presentation.theme.ColorPrimary
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun IsNewCoinBadge(isNew: Boolean) {
-    if (isNew) Box(
+fun NewCoinBadge(isNew: Boolean) {
+    if (isNew) Card(
         modifier = Modifier
-            .padding(start = 12.dp)
-            .clip(RoundedCornerShape(2.dp))
-            .background(ColorBadgeBg)
+            .padding(start = 12.dp),
+        colors = CardDefaults.cardColors(colorScheme.primary)
     ) {
-        Text(
-            modifier = Modifier.padding(horizontal = 6.dp, vertical = 2.dp),
-            text = "NEW",
-            color = ColorBadgeText,
-            fontSize = 10.sp
-        )
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                modifier = Modifier.padding(
+                    start = 6.dp,
+                    end = 6.dp,
+                    top = 2.dp,
+                    bottom = 1.dp
+                ),
+                text = "NEW",
+                style = typography.labelSmall
+            )
+        }
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CoinTypeBadge(type: String) {
-    Box(
+    Card(
         modifier = Modifier
-            .padding(start = 12.dp)
-            .clip(RoundedCornerShape(2.dp))
-            .background(
-                if (type.equals("COIN", ignoreCase = true))
-                    ColorPrimary.copy(alpha = 0.12F) else ColorBadgeBg
-            ), contentAlignment = Alignment.Center
-    ) {
-        Text(
-            modifier = Modifier.padding(
-                start = 6.dp,
-                end = 6.dp,
-                top = 2.dp,
-                bottom = 1.dp
-            ),
-            text = if (type.equals("COIN", ignoreCase = true)) "COIN" else "TOKEN",
-            color = if (type.equals(
-                    "COIN",
-                    ignoreCase = true
-                )
-            ) ColorPrimary else ColorBadgeText,
-            fontSize = 10.sp
+            .padding(start = 12.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = if (type.equals("COIN", ignoreCase = true))
+                colorScheme.secondary else colorScheme.tertiary
         )
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            Text(
+                modifier = Modifier.padding(
+                    start = 6.dp,
+                    end = 6.dp,
+                    top = 2.dp,
+                    bottom = 1.dp
+                ),
+                text = if (type.equals("COIN", ignoreCase = true)) "COIN" else "TOKEN",
+                style = typography.labelSmall
+            )
+        }
     }
 }
